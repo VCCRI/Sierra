9ce3b6c430ef9fc6542c8011e384ea59e101529d
Ralph <ralph.j.patrick@gmail.com>
2019-03-29 12:30:22 +1100

    Updates to function references

--- R/Annotate.R
+++ R/Annotate.R
@@ -5,7 +5,7 @@
 #'
 #'  gr is the genomic ranges that need to be annotation. Ideally original input should be in the format:
 #'       chr1:1234-2345:+    # chr:start-end:strand
-#'   This could already exist within an R object or you can copy it in via readClipboard. 
+#'   This could already exist within an R object or you can copy it in via readClipboard.
 #'
 #'  gr <- GRanges(readClipboard())
 #'
@@ -18,9 +18,9 @@
 #' @param gr a granges object of peaks to annotate
 #' @param invert_strand Boolean to signifiy if strand of gr peaks should be inversed
 #' @param gtf_gr granges gtf file that contains annotation information
-#' @param annotationType can be assigned "within" or "all". Default is "within" which states that the peak with gr must be within annotation feature (eg exon) 
+#' @param annotationType can be assigned "within" or "all". Default is "within" which states that the peak with gr must be within annotation feature (eg exon)
 #' @param transcriptDetails Boolean. If false will only return gene name. If true will return internal transcript position feature (eg exon/intron)
-#' @param gtf_TxDb  same as gtf_gr but as a TxDb object. 
+#' @param gtf_TxDb  same as gtf_gr but as a TxDb object.
 #' @return a dataframe with appended columns containing annotation
 ##
 ## Written March 2019
@@ -37,14 +37,14 @@ annotate_gr_from_gtf <- function(gr, invert_strand = FALSE, gtf_gr = NULL,
   { gr <- invertStrand(gr) }
 
 #  mcols(gtf_gr) <- mcols(gtf_gr)[c("type","gene_id","gene_type","gene_name")]
-  mcols(gtf_gr) <- mcols(gtf_gr)[c("type","gene_id","gene_name")]
+  GenomicRanges::mcols(gtf_gr) <- GenomicRanges::mcols(gtf_gr)[c("type","gene_id","gene_name")]
   my_genes <- gtf_gr[gtf_gr$type == "gene"]
 
 
   all_hits <- GenomicAlignments::findOverlaps(gr , my_genes, type= annotationType)
-  identified_gene_symbols <- my_genes[subjectHits(all_hits)]$gene_name
+  identified_gene_symbols <- my_genes[S4Vectors::subjectHits(all_hits)]$gene_name
 
-  idx_to_annotate <- GenomicAlignments::queryHits(all_hits)
+  idx_to_annotate <- S4Vectors::queryHits(all_hits)
   multi_annotations <- which(table(idx_to_annotate) > 1)
   unique_annotations <- unique(idx_to_annotate)
 
@@ -67,36 +67,36 @@ annotate_gr_from_gtf <- function(gr, invert_strand = FALSE, gtf_gr = NULL,
     df$UTR3 <- ""
     UTR_3_GR <- GenomicFeatures::threeUTRsByTranscript(gtf_TxDb)
     all_UTR_3_hits <- GenomicAlignments::findOverlaps(gr , UTR_3_GR,type = annotationType)
-    idx_to_annotate_3UTR <- GenomicAlignments::queryHits(all_UTR_3_hits)
+    idx_to_annotate_3UTR <- S4Vectors::queryHits(all_UTR_3_hits)
     df$UTR3[idx_to_annotate_3UTR] <- "YES"
 
     cat("\nAnnotating 5' UTRs")
     df$UTR5 <- ""
     UTR_5_GR <- GenomicFeatures::fiveUTRsByTranscript(gtf_TxDb)
     all_UTR_5_hits <- GenomicAlignments::findOverlaps(gr , UTR_5_GR,type = annotationType)
-    identified_5UTRs <- UTR_5_GR[subjectHits(all_UTR_5_hits)]$exon_id
-    idx_to_annotate_5UTR <- GenomicAlignments::queryHits(all_UTR_5_hits)
+    identified_5UTRs <- UTR_5_GR[S4Vectors::subjectHits(all_UTR_5_hits)]$exon_id
+    idx_to_annotate_5UTR <- S4Vectors::queryHits(all_UTR_5_hits)
     df$UTR5[idx_to_annotate_5UTR] <- "YES"
 
     cat("\nAnnotating introns")
     df$intron <- ""
-    introns_GR <- intronsByTranscript(gtf_TxDb)
+    introns_GR <- GenomicFeatures::intronsByTranscript(gtf_TxDb)
     all_intron_hits <- GenomicAlignments::findOverlaps(gr , introns_GR, type = annotationType)
-    identified_introns <- introns_GR[subjectHits(all_intron_hits)]
-    idx_to_annotate_introns <- GenomicAlignments::queryHits(all_intron_hits)
+    identified_introns <- introns_GR[S4Vectors::subjectHits(all_intron_hits)]
+    idx_to_annotate_introns <- S4Vectors::queryHits(all_intron_hits)
     df$intron[idx_to_annotate_introns] <- "YES"
 
     cat("\nAnnotating exons")
     my_exons <- gtf_gr[gtf_gr$type == "exon"]
     all_exon_hits <-  GenomicAlignments::findOverlaps(gr , my_exons, type = annotationType)
-    idx_to_annotate_exons <- GenomicAlignments::queryHits(all_exon_hits)
+    idx_to_annotate_exons <- S4Vectors::queryHits(all_exon_hits)
     df$exon <- ""
     df$exon[idx_to_annotate_exons] <- "YES"
 
     cat("\nAnnotating CDS")
     my_CDS <- gtf_gr[gtf_gr$type == "CDS"]
     all_CDS_hits <-  GenomicAlignments::findOverlaps(gr , my_CDS, type = annotationType)
-    idx_to_annotate_CDS <- GenomicAlignments::queryHits(all_CDS_hits)
+    idx_to_annotate_CDS <- S4Vectors::queryHits(all_CDS_hits)
     df$CDS <- ""
     df$CDS[idx_to_annotate_CDS] <- "YES"
 
