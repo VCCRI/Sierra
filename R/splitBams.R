29beb8bf99ca6560469f3851c4bc5d49d1bd54cc
Kitty Lo <kitty@maths.usyd.edu.au>
2019-08-14 14:25:47 +1000

    Added documentation to splitBam function

--- R/splitBams.R
+++ R/splitBams.R
@@ -1,10 +1,24 @@
-## Utility to split a bam file into multiple bam files based on the barcode 
-
-library(GenomicAlignments) 
-library(rtracklayer)
-
+#############################################################################
+#' 
+#' Utility to split a bam file into multiple bam files based on the barcode 
+#' 
+#' Given a bam file that was processed by CellRanger, splitBam splits the 
+#' bam into multiple bam files, one per cell barcode. 
+#' Bam file needs to have the barcode stored in the "CB" field. 
+#' 
+#' @param bam CellRanger outputted bam file with the CB field 
+#' @param cellbc.df data frame of the cell barcode, needs to have the column names: "celltype" and "cellbc" 
+#' @param outdir directory to output the bam files. The bam files will be called [celltype].bam 
+#' @param yieldSize number of lines of bam files to load. Default: 1000000 
+#' 
+#' @return 
+#' 
+#' @export 
+#' 
 splitBam <- function(bam, cellbc.df, outdir, yieldSize = 1000000) { 
    message("splitting bam file: ", bam) 
+   require(GenomicAlignments) 
+   require(rtracklayer)
   
    param <- ScanBamParam(tag=c("CB", "UB"))
   
@@ -16,7 +30,7 @@ splitBam <- function(bam, cellbc.df, outdir, yieldSize = 1000000) {
       message("outputing to ", outfile)
       aln.per.type <- NULL
       cellbc <- subset(cellbc.df, celltype == eachtype)$cellbc
-      bamfile <- BamFile(bam, yieldSize=10000000)
+      bamfile <- BamFile(bam, yieldSize=yieldSize)
       open(bamfile)
       while (length(chunk0 <- readGAlignments(bamfile,param))) {
          chunk0 <- readGAlignments(bamfile,param=param)
