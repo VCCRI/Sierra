a44fc0a2fa5ee5d7012361010a7fc3087a92b7e3
Ralph Patrick <ralph@UNKNOWN.gateway>
2019-09-28 22:05:37 +1000

    Add object creator for Single Cell Expression Set

--- R/data_util.R
+++ R/data_util.R
@@ -149,7 +149,7 @@ polya_seurat_from_gene_object <- function(peak.data, genes.seurat, annot.info, p
   }, error = function(err) {
     print("No t-SNE coodinates detected")
   })
-  
+
   ## Add UMAP coordinates to peak count object
   tryCatch({
     umap.embeddings = Embeddings(genes.seurat, reduction = 'umap')
@@ -312,7 +312,7 @@ new_polya_seurat <- function(peak.data, annot.info, project.name = "PolyA",
   print(paste("Creating Seurat object with", nrow(peak.data), "peaks and", ncol(peak.data), "cells"))
 
   ## Create a Seurat object for polyA counts
-  apa.seurat = CreateSeuratObject(peak.data, min.cells = min.cells, project = project.name)
+  apa.seurat = Seurat::CreateSeuratObject(peak.data, min.cells = min.cells, project = project.name)
 
   ## Add peak annotations to the Seurat object
   annot.info = as.data.frame(annot.info, stringsAsFactors = FALSE)
@@ -366,7 +366,7 @@ new_polya_seurat <- function(peak.data, annot.info, project.name = "PolyA",
   apa.seurat@tools <- feature.mat.input
 
   ## Normalise and calculate highly-variable genes
-  apa.seurat <- NormalizeData(object = apa.seurat, normalization.method = "LogNormalize",
+  apa.seurat <- Seurat::NormalizeData(object = apa.seurat, normalization.method = "LogNormalize",
                               scale.factor = norm.scale.factor)
 
   return(apa.seurat)
@@ -460,6 +460,113 @@ new_polya_seurat_v2 <- function(peak.data, annot.info, project.name = "PolyA",
                               scale.factor = norm.scale.factor)
 
   return(apa.seurat)
+}
+
+
+################################################
+#'
+#' Create a new peak-counts single-cell expression object from the peak counts
+#'
+#' Creates a new peak-counts single-cell expression object from the peak counts and annotation table
+#'
+#' @param peak.data matrix of peak counts
+#' @param annot.info peak annotation information
+#' @param cell.idents cell identities to be used for DU analysis
+#' @param project.name project name passed to the Seurat object creation
+#' @param min.cells minimum number of cells for retaining a peak
+#' @param min.peaks minimum number of peaks for retaining a cell
+#' @param norm.scale.factor scale factor for Seurat NormalizeData function
+#'
+#' @return a new peak-level SCE object
+#'
+#' @examples
+#' apa.sce = new_peak_SCE(apa.data, genes.seurat, annot.info)
+#'
+#' @export
+#'
+new_peak_SCE <- function(peak.data, annot.info, cell.idents, project.name = "PolyA",
+                             min.cells = 10, min.peaks = 200, norm.scale.factor = 10000) {
+
+  ## Read in annotations to add to the SCE object
+  annot.peaks = rownames(annot.info)
+
+  names(cell.idents) <- colnames(peak.data)
+
+  ## Check if there are annotations for peaks
+  peaks.use = intersect(rownames(peak.data), annot.peaks)
+  peak.data = peak.data[peaks.use, ]
+
+  print(paste("Creating SCE object with", nrow(peak.data), "peaks and", ncol(peak.data), "cells"))
+
+  ## filter peaks and cells
+  #rows.keep <- which(rowSums(peak.data > 0) >= min.cells)
+  nz.row.counts <- tabulate(peak.data@i + 1, nbins = nrow(peak.data))
+  peaks.keep <- rownames(peak.data)[which(nz.row.counts >= min.cells)]
+
+  nz.col.counts <- diff(apa.data@p)
+  cells.keep <- colnames(peak.data)[which(nz.col.counts >= min.peaks)]
+
+  ## filter the matrix and corresponding cell identities
+  peak.data <- peak.data[peaks.keep, cells.keep]
+  cell.idents <- cell.idents[cells.keep]
+
+  ## Create an SCE object for peak counts
+  peaks.sce <- SingleCellExperiment::SingleCellExperiment(assays = list(counts = apa.data))
+
+  ## Add peak annotations to the SCE object
+  annot.info = as.data.frame(annot.info, stringsAsFactors = FALSE)
+
+  peaks.use = intersect(annot.peaks, rownames(peaks.sce))
+  annot.info = annot.info[peaks.use, ]
+  feature.names = c("UTR3", "UTR5", "intron", "exon")
+  feature.mat = annot.info[peaks.use, feature.names]
+
+  features.collapsed = apply(feature.mat, 1, function(x) {
+    paste(feature.names[which(x == "YES")], collapse = ";")})
+
+  feature.mat$FeaturesCollapsed = features.collapsed
+  feature.mat$Gene_name = annot.info$gene_id
+  feature.mat$start = annot.info$start
+  feature.mat$end = annot.info$end
+  feature.mat$chr = annot.info$seqnames
+  feature.mat$strand = annot.info$strand
+
+  ## Add additional peak IDs for input to DEXSeq
+  print("Preparing feature table for DEXSeq")
+  gene.set = unique(as.character(feature.mat$Gene_name))
+  dexseq.feature.table = c()
+  for (this.gene in gene.set) {
+
+    ## collect peaks
+    peak.subset = subset(feature.mat, Gene_name == this.gene)
+    peak.subset = peak.subset[order(peak.subset$start, decreasing = FALSE), ]
+
+    transcript.names = paste0('transcripts "', rownames(peak.subset), '"')
+    gene.ids = paste0('gene_id "', peak.subset$gene_id, '"')
+    exonic.part.numbers = paste0('exonic_part_number "', 1:nrow(peak.subset), '"')
+    info.part = paste(transcript.names, exonic.part.numbers, gene.ids, sep = "; ")
+
+    dexseq.feature.set = data.frame(Gene_name = peak.subset$Gene_name,
+                                    Gene_part = paste0(peak.subset$Gene_name, ":", 1:nrow(peak.subset)),
+                                    Peak_number = paste0("Peak", 1:nrow(peak.subset)),
+                                    Peak_name = rownames(peak.subset), stringsAsFactors = FALSE)
+    rownames(dexseq.feature.set) <- dexseq.feature.set$Peak_name
+
+    dexseq.feature.table = rbind(dexseq.feature.table, dexseq.feature.set)
+  }
+  dexseq.feature.table = dexseq.feature.table[rownames(feature.mat), ]
+
+  feature.mat$Gene_part = dexseq.feature.table$Gene_part
+  feature.mat$Peak_number = dexseq.feature.table$Peak_number
+
+  ## Store the data in the SCE @metadata slot
+  peaks.sce@metadata$GeneSLICER <- feature.mat
+
+  ## Add cell annotation information
+  cell.data <- DataFrame(CellIdent = cell.idents)
+  SingleCellExperiment::colData(peaks.sce) <- cell.data
+
+  return(peaks.sce)
 }
 
 
