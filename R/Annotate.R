3762702eb36751f79e8900e34a946acbd52fd940
David Humphreys <d.humphreys@victorchang.edu.au>
2020-05-25 16:46:29 +1000

    Added additional coordinate check in BaseComposition function

--- R/Annotate.R
+++ R/Annotate.R
@@ -536,6 +536,12 @@ BaseComposition <- function(genome=NULL,  chrom=NULL, start=NULL, stop=NULL, str
       stop <- as.numeric(coord_detail[[1]][2])
     }
   }
+  
+  if (! is.numeric(start)) | (! is.numeric(stop))
+  {
+    warning("Could not define/identify start stop coordinates. Cannot retrieve sequence")
+    return(NULL)
+  }
 
   # Require start to be smaller number
   if ( start > stop)
